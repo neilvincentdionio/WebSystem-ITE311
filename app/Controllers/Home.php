c3a3fee3b015ab82e979a6c5b36a1deddb3a8f1e
Neil Vincent Dionio <neilvincentdionio@gmail.com>
2025-08-08 00:27:55 +0800

    Added Bootstrap template and Home controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,8 +1,11 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
-    public function index() {
-        $this->load->view('template');
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        return view('template');
     }
 }
